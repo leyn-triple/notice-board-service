ebd737470561c1851cdf6ff9bb2b2776988f0b51
leyn-triple <leyn.triple@gmail.com>
2023-11-09 20:33:48 +0900

    Initial commit

---
+++ src/main/java/com/triple/board/NoticeBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.triple.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NoticeBoardServiceApplication.class, args);
+  }
+
+}
